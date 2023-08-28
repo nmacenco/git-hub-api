a7c6fc03113ab9b65451a3cb92902404c6593a87
nmacenco <nicolasmacenco@gmail.com>
2023-08-28 12:10:38 -0300

    feat(client): added spinner to commits and profile pages

--- client/src/pages/commits/index.tsx
+++ client/src/pages/commits/index.tsx
@@ -1,34 +1,38 @@
+import { Box, CircularProgress, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { gitHubInfo } from "../../api/github.api";
+import { FolderList } from "../../components/FolderList";
 
-
-import { Box, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { gitHubInfo } from '../../api/github.api';
-import { FolderList } from '../../components/FolderList';
-
-export const Commits : React.FC<{}> = () => {
-
-  const [commitsList, setcommitsList] = useState([])
-  useEffect(()=>{
-    
+export const Commits: React.FC<{}> = () => {
+  const [commitsList, setcommitsList] = useState([]);
+  const [spinner, setSpinner] = useState(true);
+  useEffect(() => {
     try {
-      gitHubInfo.getAllCommits().then((response) =>{
-        setcommitsList(response.data)
-      })
-      
+      gitHubInfo
+        .getAllCommits()
+        .then((response) => {
+          setcommitsList(response.data);
+        })
+        .finally(() => {
+          setSpinner(false);
+        });
     } catch (error) {
       console.log(error);
-      
     }
-  }, [])
-  
+  }, []);
+
   return (
-      <>
-        <Typography variant="h4" sx={{marginBottom : '15px'}}>Commit List:</Typography>
-        <Box sx={{display:'flex' , justifyContent: 'center' }} >
-          {commitsList.length > 0 && <FolderList items= {commitsList}  />}
-        </Box >
-      </>
-
+    <>
+      <Typography variant="h4" sx={{ marginBottom: "15px" }}>
+        Commit List:
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        {spinner ? (
+          <CircularProgress color="primary" />
+        ) : (
+          <>{commitsList.length > 0 && <FolderList items={commitsList} />}</>
+        )}
+      </Box>
+    </>
   );
-}
-
+};
--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -1,41 +1,45 @@
-import React, { useEffect , useLayoutEffect, useState } from 'react';
-import '../../App.css';
-import { MediaCard, MediaCardProps } from '../../components/Card';
-import { Box } from '@mui/material';
-import { gitHubInfo } from '../../api/github.api';
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import "../../App.css";
+import { MediaCard, MediaCardProps } from "../../components/Card";
+import { Box, CircularProgress } from "@mui/material";
+import { gitHubInfo } from "../../api/github.api";
 
-
-export const Profile : React.FC<{}> = () => {
-  const [profileInfo, setProfileInfo] = useState<MediaCardProps | null>(null)
-  useLayoutEffect(()=>{
-    
+export const Profile: React.FC<{}> = () => {
+  const [profileInfo, setProfileInfo] = useState<MediaCardProps | null>(null);
+  const [spinner, setSpinner] = useState(true);
+  useLayoutEffect(() => {
     try {
-      gitHubInfo.getProfileInfo().then((response) =>{
-        setProfileInfo(response.data)
+      gitHubInfo.getProfileInfo().then((response) => {
+        setProfileInfo(response.data);
       })
-      
+      .finally(() => {
+        setSpinner(false);
+      });
     } catch (error) {
       console.log(error);
-      
     }
-  }, [])
-  
-  return (
-    <Box sx={{display:'flex' , justifyContent: 'center' }} >
-      { 
-        profileInfo !== null && 
-          <MediaCard   
-            avatar ={ profileInfo.avatar}  
-            description ={ profileInfo.description}  
-            fullName ={ profileInfo.fullName}  
-            language ={ profileInfo.language}  
-            name ={ profileInfo.name}  
-            owner ={ profileInfo.owner}  
-            profileUrl ={ profileInfo.profileUrl}  
-            repoUrl ={ profileInfo.repoUrl}  
-          />
-      }
-    </Box >
-  );
-}
+  }, []);
 
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      {spinner ? (
+        <CircularProgress color="primary" />
+      ) : (
+        <>
+          {profileInfo !== null && (
+            <MediaCard
+              avatar={profileInfo.avatar}
+              description={profileInfo.description}
+              fullName={profileInfo.fullName}
+              language={profileInfo.language}
+              name={profileInfo.name}
+              owner={profileInfo.owner}
+              profileUrl={profileInfo.profileUrl}
+              repoUrl={profileInfo.repoUrl}
+            />
+          )}
+        </>
+      )}
+    </Box>
+  );
+};
